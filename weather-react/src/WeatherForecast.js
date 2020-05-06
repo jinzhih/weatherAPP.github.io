af2c2bd558af45e3802502714fcb52f82a68b4e9
Jessie Hou <jessie.houjinzhi@gmail.com>
2020-05-06 23:20:56 +1000

    update click five or ten items

--- weather-react/src/WeatherForecast.js
+++ weather-react/src/WeatherForecast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import classNames from 'classnames'
 
 import { format } from "date-fns";
 
@@ -8,14 +9,41 @@ import ForecastRow from "./ForecastRow";
 class WeatherForecast extends React.Component {
   state = {
     forecasts: [],
+    addClass:true,
+    item:5,
   };
+
+  changeItemsTen = () => {
+    this.setState({addClass: false});
+    this.setState({item: 10});
+    axios(
+        "https://jr-weather-api.herokuapp.com/api/weather?cc=au&city=brisbane"
+      ).then((res) => {
+        //   console.log(res);
+        const forecasts = res.data.data.forecast.slice(0, this.state.item);
+        this.setState({ forecasts });
+      });
+  
+  }
+  changeItemsFive =() => {
+    this.setState({addClass: true}); 
+    this.setState({item: 5});
+    axios(
+        "https://jr-weather-api.herokuapp.com/api/weather?cc=au&city=brisbane"
+      ).then((res) => {
+        //   console.log(res);
+        const forecasts = res.data.data.forecast.slice(0, this.state.item);
+        this.setState({ forecasts });
+      });
+  
+  }
 
   componentDidMount() {
     axios(
       "https://jr-weather-api.herokuapp.com/api/weather?cc=au&city=brisbane"
     ).then((res) => {
       //   console.log(res);
-      const forecasts = res.data.data.forecast.slice(0, 10);
+      const forecasts = res.data.data.forecast.slice(0, this.state.item);
       this.setState({ forecasts });
     });
   }
@@ -24,8 +52,9 @@ class WeatherForecast extends React.Component {
     return (
       <section className="weather-forecast">
         <div className="forecast__switch">
-          <button className="forecast__switch_0 switch-active">5 items</button>
-          <button className="forecast__switch_1">10 items</button>
+        <button onClick={this.changeItemsFive} className={classNames({"forecast__switch_0":true,"switch-active":this.state.addClass})}>5 items</button>
+        <button onClick={this.changeItemsTen} className={classNames({"forecast__switch_1":true,"switch-active":(!this.state.addClass)})}>10 items</button>  
+          
         </div>
         {this.state.forecasts.map((forecast) => {
           const date = new Date(forecast.time * 1000);
